6eed62d8f9a079668da9b7f8b06c5a59eda85b6d
mfrouh <mohamedfrouh96@gmail.com>
2022-09-17 18:11:35 +0200

    import storage in product resource

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Storage;
 
 class ProductResource extends JsonResource
 {
